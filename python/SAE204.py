0460415d35e9a4a5260e6044b89792bd635d7fe9
SofienZeggane <sofien.zeggane@etu.univ-amu.fr>
2025-03-28 17:15:36 +0100

    Deux premier diagrammes avec justification

--- python/SAE204.py
+++ python/SAE204.py
@@ -8,28 +8,10 @@ Bien = pandas.read_csv("BIEN_DATA_TABLE.csv")
 Espace = pandas.read_csv("ESPACE_DATA_TABLE.csv")
 Proprio = pandas.read_csv("PROPRIO_DATA_TABLE.csv")
 
-#Affichage de la table
-
-print("Affichage de la table Bien")
-print(Bien)
-print("Affichage de la table Espace")
-print(Espace)
-print("Affichage de la table Proprio")
-print(Proprio)
-
-#Nombre de biens par ville
-
-print(Bien["VILLE"].value_counts())
-
 #Prix moyen des bien
 
 print("Prix moyen des biens")
 print(Bien["PRIX"].mean())
-
-#Prix moyen des Villa
-
-Nombre_de_villa = Bien.loc[Bien["CATEGORIE"] == "Villa"]
-print("le prix moyen des villa est", Nombre_de_villa["PRIX"].mean())
 
 #Diagramme en camambert sur le type de bien
 
@@ -38,8 +20,21 @@ Cat = Bien.groupby("CATEGORIE")["CATEGORIE"].count()
 Cat.plot.pie(autopct = lambda z : str(round(z, 2)) + '%', pctdistance = 0.6)
 plt.show()
 
-#Diagramme en bande de la moyenne du prix pas type de bien
+print("Le choix d'un diagramme en camambert ici est pertinent car un diagramme \
+      en camambert est très comprehensible et lisible lorsque il y a peu de differentes\
+      valeurs ici le nombre de categorie n'est pas specialement élévée\
+      De plus le diagramme camambert est pertinent lorsque les valeurs sont représenter\
+      en % comme ici")
+
+#Diagramme en bande de la moyenne du prix par type de bien
 
 print("Diagramme en bande de la moyenne du prix par type de bien")
 Cat2 = Bien.groupby("CATEGORIE")["PRIX"].mean()
-Cat2.plot.bar(width = 0.5, edgecolor = 'black')
+Diag = Cat2.plot.bar(width = 0.5, edgecolor = 'black')
+Diag.set_ylabel("Prix moyen en €")
+plt.show()
+
+print("Le choix d'un diagramme en bande pour ces informations \
+      est pertinent ici car il permet une comparaison très compréhensible \
+      entre les differentes categorie")
+
