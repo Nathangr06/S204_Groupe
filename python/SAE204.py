054e728bc5a3d8a62ef0e71c4f2aad71ed34661b
Nathangr06 <nathan.griguer@etu.univ-amu.fr>
2025-03-29 13:05:23 +0100

    maj programme python

--- python/SAE204.py
+++ python/SAE204.py
@@ -15,8 +15,8 @@ print(Bien["PRIX"].mean())
 
 #Diagramme en camambert sur le type de bien
 
-print("Diagramme en camambert sur les differents types de bien")
 Cat = Bien.groupby("CATEGORIE")["CATEGORIE"].count()
+plt.title("Répartition des differents types de bien pour tous les propriétaires")
 Cat.plot.pie(autopct = lambda z : str(round(z, 2)) + '%', pctdistance = 0.6)
 plt.show()
 
@@ -25,12 +25,14 @@ print("Le choix d'un diagramme en camambert ici est pertinent car un diagramme \
       valeurs ici le nombre de categorie n'est pas specialement élévée\
       De plus le diagramme camambert est pertinent lorsque les valeurs sont représenter\
       en % comme ici")
+print()
+
 
 #Diagramme en bande de la moyenne du prix par type de bien
 
-print("Diagramme en bande de la moyenne du prix par type de bien")
 Cat2 = Bien.groupby("CATEGORIE")["PRIX"].mean()
 Diag = Cat2.plot.bar(width = 0.5, edgecolor = 'black')
+plt.title("Prix moyen par type de bien")
 Diag.set_ylabel("Prix moyen en €")
 plt.show()
 
@@ -38,3 +40,19 @@ print("Le choix d'un diagramme en bande pour ces informations \
       est pertinent ici car il permet une comparaison très compréhensible \
       entre les differentes categorie")
 
+#Corrélation entre le prix et la surface des biens (corriger calcul des coeffs)
+Jointure_Bien_Espace = pandas.concat([Bien, Espace], axis = 1)
+Corr = Jointure_Bien_Espace["PRIX"].corr(Jointure_Bien_Espace["SUPERFICIE"])
+
+plt.scatter(Jointure_Bien_Espace["SUPERFICIE"], Jointure_Bien_Espace["PRIX"])
+plt.title("Corrélation entre le prix et la surface")
+plt.xlabel("Superficie")
+plt.ylabel("Prix")
+
+coeffs= np.polyfit(Jointure_Bien_Espace["SUPERFICIE"], Jointure_Bien_Espace["PRIX"], 1)
+print(coeffs)
+plt.plot(Jointure_Bien_Espace["SUPERFICIE"], coeffs[0] * Jointure_Bien_Espace["SUPERFICIE"] + coeffs[1], color='red')
+plt.show()
+print("La corrélation entre le prix et la surface est de : ", Corr)
+
+#Faire diagramme en boite des prix des différents biens
